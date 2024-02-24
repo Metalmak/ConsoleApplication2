8990767e8022f921350a4625465c9cf2184c1f54
MAKS <metalmak@mail.ru>
2024-02-24 22:19:28 +0500

    Initial commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person tom = new Person();
+        }
+
+        class Person
+        {
+            public string name = "Underfined";
+            public int age;
+
+            public void Print()
+            {
+                Console.WriteLine($"Name : {name} Age : {age}");
+            }
+        }
+    }
+}
